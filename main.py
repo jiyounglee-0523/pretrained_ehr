4b88e5d18bf874c0b0751b659488e9ebae37055b
unknown <jiyounglee0523@gmail.com>
2020-12-22 22:19:16 +0900

    Dec 22

---
+++ main.py
@@ -0,0 +1,45 @@
+import torch
+
+import random
+import numpy as np
+import argparse
+
+from dataset.dataloader import get_dataloader
+from trainer.base_trainer import Trainer
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--source_file', choices=['mimic', 'eicu', 'both'], type=str, default='eicu')
+    parser.add_argument('--target', choices=['readmission', 'mortality', 'los>3day', 'los>7day', 'diagnosis'], type=str, default='readmission')
+    parser.add_argument('--item', choices=['lab', 'diagnosis','chartevent','medication','infusion'], type=str, default='lab')
+    parser.add_argument('--time_window', choices=['12', '24', '36', '48', 'Total'], type=str, default='12')
+    parser.add_argument('--model_type', choices=['gru', 'lstm'], type=str, default='gru')
+    parser.add_argument('--batch_size', type=int, default=512)
+    parser.add_argument('--embedding_dim', type=int, default=128)
+    parser.add_argument('--hidden_dim', type=int, default=128)
+    parser.add_argument('--rnn_bidirection', type=bool, default=True)
+    parser.add_argument('--n_epochs', type=int, default=5)
+    parser.add_argument('--lr', type=float, default=1e-3)
+    parser.add_argument('--max_length', type=str, default='150')
+    args = parser.parse_args()
+
+
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    SEED = 1234
+    random.seed(SEED)
+    np.random.seed(SEED)
+    torch.manual_seed(SEED)
+    torch.cuda.manual_seed(SEED)
+    torch.backends.cudnn.deterministic = True
+
+    train_loader = get_dataloader(args, data_type='train')
+    valid_loader = get_dataloader(args, data_type='eval')
+
+    trainer = Trainer(args, train_loader, valid_loader, device)
+    trainer.train()
+
+
+if __name__ == '__main__':
+    main()
---
+++ trainer/base_trainer.py
@@ -0,0 +1,127 @@
+import torch
+import torch.nn as nn
+from sklearn.metrics import roc_auc_score, average_precision_score
+from models.rnn_models import *
+
+from utils.loss import *
+
+class Trainer(nn.Module):
+    def __init__(self, args, train_dataloader, valid_dataloader, device):
+        super().__init__()
+
+        self.dataloader = train_dataloader
+        self.eval_dataloader = valid_dataloader
+        self.criterion = FocalLoss()
+        self.device = device
+
+        lr = args.lr
+        self.n_epochs = args.n_epochs
+        self.path = './test.pt'      ########## change this!
+
+        if args.source_file == 'mimic':
+            vocab_size = 449
+        elif args.source_file == 'eicu':
+            vocab_size = 150
+        else:
+            raise NotImplementedError
+
+        if args.item == 'diagnosis':
+            output_size = 17
+        else:
+            output_size = 1
+
+        self.model = RNNmodels(args=args, vocab_size=vocab_size, output_size=output_size, device=device).to(self.device)
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=lr)
+
+
+
+    def train(self):
+        best_loss = float('inf')
+        best_auroc = 0.0
+        best_auprc = 0.0
+        for n_epoch in range(self.n_epochs):
+
+            preds_train = []
+            truths_train = []
+            avg_train_loss = 0.
+
+            for iter, sample in enumerate(self.dataloader):
+                self.model.train()
+                self.optimizer.zero_grad()
+
+                item_id, item_offset, item_offset_order, lengths, target = sample
+                item_id.to(self.device) ; item_offset.to(self.device) ; item_offset_order.to(self.device)
+                lengths.to(self.device) ; target.to(self.device)
+
+                y_pred = self.model(item_id, lengths)
+                loss = self.criterion(y_pred, target.float().to(self.device))
+
+                loss.backward()
+                self.optimizer.step()
+
+                avg_train_loss += loss.item() / len(self.dataloader)
+
+                probs_train = F.sigmoid(y_pred).detach().cpu().numpy()
+                preds_train += list(probs_train.flatten())
+                truths_train += list(target.detach().cpu().numpy().flatten())
+
+            auroc_train = roc_auc_score(truths_train, preds_train)
+            auprc_train = average_precision_score(truths_train, preds_train)
+
+            avg_eval_loss, auroc_eval, auprc_eval = self.evaluation()
+
+            if best_loss > avg_eval_loss:
+                best_loss = avg_eval_loss
+                best_auroc = auroc_eval
+                best_auprc = auprc_eval
+                torch.save({'model_state_dict': self.model.state_dict(),
+                            'optimizer_state_dict': self.optimizer.state_dict(),
+                            'loss': avg_eval_loss,
+                            'auroc': best_auroc,
+                            'auprc': best_auprc}, self.path)
+
+                print('Model parameter saved at epoch {}'.format(n_epoch))
+
+            if n_epoch % 20 == 0:
+                print('[Train]  loss: {:.3f},     auroc: {:.3f},     auprc:   {:.3f}'.format(avg_train_loss, auroc_train, auprc_train))
+                print('[Valid]  loss: {:.3f},     auroc: {:.3f},     auprc:   {:.3f}'.format(avg_eval_loss, auroc_eval, auprc_eval))
+
+
+
+    def evaluation(self):
+        self.model.eval()
+
+        preds_eval = []
+        truths_eval = []
+        avg_eval_loss = 0.
+
+        with torch.no_grad():
+            for iter, sample in enumerate(self.eval_dataloader):
+                item_id, item_offset, item_offset_order, lengths, target = sample
+                item_id.to(self.device);
+                item_offset.to(self.device);
+                item_offset_order.to(self.device)
+                lengths.to(self.device);
+                target.to(self.device)
+
+                y_pred = self.model(item_id, lengths)
+                loss = self.criterion(y_pred, target.float().to(self.device))
+                avg_eval_loss += loss.item() / len(self.eval_dataloader)
+
+                probs_eval = F.sigmoid(y_pred).detach().cpu().numpy()
+                preds_eval += list(probs_eval.flatten())
+                truths_eval += list(target.detach().cpu().numpy().flatten())
+
+            auroc_eval = roc_auc_score(truths_eval, preds_eval)
+            auprc_eval = average_precision_score(truths_eval, preds_eval)
+
+            return avg_eval_loss, auroc_eval, auprc_eval
+
+
+
+
+
+
+
+
+
