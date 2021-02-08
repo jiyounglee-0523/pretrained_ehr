a3ca664102f169b657106cc0a24aa24fd7da2e23
unknown <jiyounglee0523@gmail.com>
2021-02-09 05:57:22 +0900

    v9.3

--- run/bert_tiny_both_CLSfreeze.py
+++ run/bert_tiny_both_CLSfreeze.py
@@ -6,24 +6,28 @@ import os
 PATH = '/home/jylee/pretrained_ehr/rnn_model/'
 SRC_PATH = PATH+'main.py'
 
-device = 6
+device = 0
 os.environ['CUDA_VISIBLE_DEVICES'] = str(device)
 
 
-target_list = ['readmission', 'mortality', 'los>3day', 'los>7day', 'dx_depth1_unique']
+target_list = ['readmission']
 
 for target in target_list:
    TRAINING_CONFIG = {
        "bert_induced": True,
-       "source_file": 'both',
-       "item": 'lab',
-       "max_length": 150,
+       "source_file": 'eicu',
+       "item": 'all',
+       "batch_size": 512,
        "bert_freeze": True,
        "target": target,
+       "lr":1e-4,
        "bert_model": 'bio_clinical_bert',
        "device_number": device,
-       #"concat" :True,
-       "cls_freeze": True
+       "cls_freeze": True,
+       "input_path": '/data/private/input_data/',
+       "path": '/data/private/KDD_output2/',
+       "only_BCE": True,
+       "wandb_project_name": 'cls_fixed_train',
    }
 
    TRAINING_CONFIG_LIST = ["--{}".format(k) if (isinstance(v, bool) and (v)) else "--{}={}".format(k,v) for (k,v) in list(TRAINING_CONFIG.items())]
